313ad45fe0ee357cbe71ba21afefc1dec30b323b
JordanEngland <lcplengland@gmail.com>
2023-04-20 15:02:37 -0500

    completed the javaScript and it is working now

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ document.addEventListener('DOMContentLoaded', function() {
   updateCurrentDateTime();
   setInterval(updateCurrentDateTime, 1000);
 
+  const currentHour = new Date().getHours();
+
+  const timeBlocks = document.getElementsByClassName('time-block');
+
+  for (let i = 0; i < timeBlocks.length; i++) {
+    const timeBlock = timeBlocks[i];
+    const hour = parseInt(timeBlock.id.split('-')[1]);
+    const textarea = timeBlock.querySelector('.description');
+
+    if (hour < currentHour) {
+      timeBlock.classList.add('past');
+      timeBlock.classList.remove('present', 'future');
+    } else if (hour === currentHour) {
+      timeBlock.classList.add('present');
+      timeBlock.classList.remove('past', 'future');
+    } else {
+      timeBlock.classList.add('future');
+      timeBlock.classList.remove('past', 'present');
+    }
+  }
+  
+  $('.saveBtn').on('click', function() {
+    const hour = $(this).parent().attr('id');
+    const description = $(this).siblings('.description').val();
+    localStorage.setItem(hour, description);
+  });
+
+  for (let i = 0; i < timeBlocks.length; i++) {
+    const timeBlock = timeBlocks[i];
+    const hour = timeBlock.id;
+    const description = localStorage.getItem(hour);
+
+    if (description) {
+      timeBlock.querySelector('.description').value = description;
+    }
+  }
+
+  $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
+});
+
